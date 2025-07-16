ff81b21ac1bb2440f4917767aa9e4530221cc92b
Michalis Giannoudis <michalis221@gmail.com>
2025-07-16 11:44:44 +0300

    finished sign in page for desktop and tablet

--- front-end/src/components/SignIn.component.tsx
+++ front-end/src/components/SignIn.component.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
 
 export const SignIn = () => {
-    return (
-      <div className="bg-white relative">
-        <div className="w-screen h-screen relative z-1">
-          <Image src="/auth-background.jpg" alt="Logo" fill className="object-contain"
-          />
+  return (
+    <div className="bg-white relative">
+      <div className="w-screen h-screen relative z-1">
+        <Image src="/auth-background.jpg" alt="Logo" fill className="object-contain"
+        />
+      </div>
+      <div className="absolute inset-0 flex flex-col justify-center items-center z-2 top-[35%] md:top-[25%] bottom-auto">
+        <div className="grid grid-cols-1 gap-2 md:gap-4">
+          <p className="text-center text-black text-lg md:text-4xl font-bold mb-4 md:mb-6 cursor-default">Note Me Up</p>
+          <div className="flex flex-col gap-4">
+            <input type="email" placeholder="Email" className="border border-black rounded-lg p-2 w-40 h-7 md:w-80 md:h-10 focus:outline-none focus:ring-1 focus:ring-black"/>
+            <input type="password" placeholder="Password" className="border border-black rounded-lg mb-4 md:mb-6 p-2 w-40 h-7 md:w-80 md:h-10 focus:outline-none focus:ring-1 focus:ring-black"/>
+            <button className="bg-indigo-600 text-white rounded-lg p-2 w-40 md:w-80 hover:bg-blue-600 transition duration-200"> Sign In </button>
+            <div className="flex items-center">
+              <div className="flex-grow h-px bg-black" />
+              <span className="mx-2 text-black text-lg"> or</span>
+              <div className="flex-grow h-px bg-black" />
+            </div>
+            <button className="bg-indigo-600 text-white rounded-lg p-2 w-40 md:w-80 hover:bg-blue-600 transition duration-200"> Sign Up </button>
+          </div>
         </div>
-        <div className="absolute flex flex-col justify-center items-center top-2/7 left-3/7 z-2 transform ">
-          <h1 className="text-center text-black text-2xl font-bold">Note Me Up</h1>
-          <h1 className="text-center text-black text-2xl font-bold">Sign In</h1>
-        </div>
-      </div>  
-    )
+      </div>
+  </div>
+  )
 }
