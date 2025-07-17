34d9e28b9e7ca9675b1b103d4f435258a91f499f
Michalis Giannoudis <michalis221@gmail.com>
2025-07-17 13:44:56 +0300

    added user model

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database.js';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  tableName: 'users',
+  timestamps: true,
+});
+
+export { User };
